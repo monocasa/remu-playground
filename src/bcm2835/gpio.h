e38587282df78e9734f10e23f0d7d6f01bebb881
monocasa <tristan.hastings.miller@gmail.com>
2014-09-13 16:29:50 -0600

    Cleanup

--- src/bcm2835/gpio.h
+++ src/bcm2835/gpio.h
@@ -25,40 +25,6 @@ public:
   Gpio(Ui &ui, Memory &mem);
   virtual ~Gpio();
 
-  uint64_t readIo(uint64_t offset, unsigned int size) override final;
-  void writeIo(uint64_t offset, uint64_t val, unsigned int size) override final;
-
-  /**
-   * Checks whether a given address is a GPIO port
-   */
-  static bool isGpioAddress(uint32_t addr) {
-    addr &= ~0x3;
-    if (addr < GPIO_BASE || GPIO_UDCLK1 < addr)
-    {
-      return false;
-     }
-
-    switch (addr)
-    {
-      case GPIO_FSEL0 ... GPIO_FSEL5:
-      case GPIO_SET0 ... GPIO_SET1:
-      case GPIO_CLR0 ... GPIO_CLR1:
-      case GPIO_LEV0 ... GPIO_LEV1:
-      case GPIO_EDS0 ... GPIO_EDS1:
-      case GPIO_REN0 ... GPIO_REN1:
-      case GPIO_FEN0 ... GPIO_FEN1:
-      case GPIO_HEN0 ... GPIO_HEN1:
-      case GPIO_LEN0 ... GPIO_LEN1:
-      case GPIO_AREN0 ... GPIO_AREN1:
-      case GPIO_AFEN0 ... GPIO_AFEN1:
-      case GPIO_PUD:
-      case GPIO_UDCLK0 ... GPIO_UDCLK1:
-        return true;
-      default:
-        return false;
-    }
-  }
-
   /**
    * Sets the bits of a particular gpio port
    */
@@ -124,6 +90,9 @@ private:
   Ui     &ui;
   Memory &mem;
   std::vector<gpio_port_t> ports;
+
+  uint64_t readIo(uint64_t offset, unsigned int size) override final;
+  void writeIo(uint64_t offset, uint64_t val, unsigned int size) override final;
 };
 
 } /*namespace remu*/
