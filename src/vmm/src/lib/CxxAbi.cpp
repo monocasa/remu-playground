067d9be9210df8fe7af40063295d175a91284f36
monocasa <tristan.hastings.miller@gmail.com>
2015-02-16 15:05:38 -0700

    Add a basic implementation for __cxa_pure_virtual

--- src/vmm/src/lib/CxxAbi.cpp
+++ src/vmm/src/lib/CxxAbi.cpp
@@ -1,7 +1,12 @@
+#include <os/Board.h>
+
+#include <cstdio>
 #include <cstdlib>
 
 extern "C" void __cxa_pure_virtual()
 {
+	printf("__cxa_pure_virtual() called.  Probably a bad build.\n");
+	os::board::shutdown();
 }
 
 void* operator new(size_t size)
