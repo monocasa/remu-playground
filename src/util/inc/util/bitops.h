cf37fa04b359a2bf8c8ab5a802a398a2e7f9558d
monocasa <tristan.hastings.miller@gmail.com>
2015-06-24 23:11:08 -0600

    Migrate some remu/util functions into libutil

--- src/util/inc/util/bitops.h
+++ src/util/inc/util/bitops.h
@@ -2,8 +2,39 @@
 #define REMU_UTIL_BITOPS_H
 
 #include <climits>
+#include <cstdint>
 
 namespace remu { namespace util { namespace bitops {
+
+template<typename T> static inline T byteSwap(T value);
+
+template<>
+uint16_t byteSwap<uint16_t>(uint16_t value)
+{
+  return (value >> 8) | (value << 8);
+}
+
+template<>
+uint32_t byteSwap<uint32_t>(uint32_t value)
+{
+  return ((uint32_t)byteSwap<uint16_t>(value) << 16) | byteSwap<uint16_t>(value >> 16);
+}
+
+template<>
+uint64_t byteSwap<uint64_t>(uint64_t value)
+{
+  return ((uint64_t)byteSwap<uint32_t>(value) << 32) | byteSwap<uint32_t>(value >> 32);
+}
+
+template<typename T>
+static inline T rotateLeft(const T x, int d) 
+{
+	const int NUM_T_BITS = sizeof(T) * CHAR_BIT;
+	const int T_BITS_MASK = NUM_T_BITS - 1;
+	const int REAL_SH = d & T_BITS_MASK;
+
+	return (x << REAL_SH) | (x >> (NUM_T_BITS - REAL_SH));
+}
 
 template<typename T>
 static inline T rotateRight(const T x, int d)
--- src/util/test/Test_bitops.cpp
+++ src/util/test/Test_bitops.cpp
@@ -6,6 +6,33 @@
 
 using namespace remu::util::bitops;
 
+TEST(bitops, byteSwap16)
+{
+	uint16_t value = byteSwap<uint16_t>(0xFEDC);
+	EXPECT_EQ(0xDCFE, value);
+}
+
+TEST(bitops, byteSwap32)
+{
+	uint32_t value = byteSwap<uint32_t>(0xFEDCBA98);
+	EXPECT_EQ(0x98BADCFE, value);
+}
+
+TEST(bitops, byteSwap64)
+{
+	uint64_t value = byteSwap<uint64_t>(0xFEDCBA9876543210UL);
+	EXPECT_EQ(0x1032547698BADCFEUL, value);
+}
+
+TEST(bitops, rotateLeft)
+{
+	uint32_t value = rotateLeft<uint32_t>(0xAAAAAAAA, 1);
+	EXPECT_EQ(0x55555555, value);
+
+	value = rotateLeft<uint32_t>(0xFF000000, 8);
+	EXPECT_EQ(0xFF, value);
+}
+
 TEST(bitops, rotateRight)
 {
 	uint32_t value = rotateRight<uint32_t>(0x55555555, 1);
